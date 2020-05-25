d615545ac887673e14514523b4b81e6f35de711c
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-05-25 23:31:26 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -4,8 +4,12 @@
         render json: birds
       end
      
-      def show
-        bird = Bird.find_by(id: params[:id])
-        render json: bird
+       def show
+      bird = Bird.find_by(id: params[:id])
+      if bird
+        render json: { id: bird.id, name: bird.name, species: bird.species }
+      else
+        render json: { message: 'Bird not found' }
       end
+    end
     end
